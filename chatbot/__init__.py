03a56fe888c19f16c6b8e172f3ff4f535c3710d9
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-06-10 15:40:42 +0700

    Create __init__.py

---
+++ chatbot/__init__.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, errors
+
+
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+TOKEN + Config.TOKEN
+
+pbot = Client("Chatbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
