ddf87ac91fa621ff248e1f0db5035479f32b94b1
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-06-10 18:41:29 +0700

    Nytet

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,10 +1,18 @@
 from pyrogram import Client, errors
+from chatbot.config import Config
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-API_ID = Config.APP_ID
+
+APP_ID = Config.APP_ID
 API_HASH = Config.API_HASH
 TOKEN = Config.TOKEN
 BOT_ID = Config.BOT_ID
 OWNER_ID = Config.OWNER_ID
 
-cb = Client("Chatbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+cb = Client("Chatbot", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
