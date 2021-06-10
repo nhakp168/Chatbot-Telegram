2f88bce405285a1d3cab8bd2149fe79768130cc3
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-06-10 16:55:58 +0700

    Update __init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -5,5 +5,6 @@ API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 TOKEN = Config.TOKEN
 BOT_ID = Config.BOT_ID
+OWNER_ID = Config.OWNER_ID
 
 cb = Client("Chatbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
