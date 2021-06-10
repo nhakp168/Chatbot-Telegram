5d33655d192d2945cdf3b9471711d7e38db8c0df
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-06-10 16:42:31 +0700

    Update __init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -3,6 +3,7 @@ from pyrogram import Client, errors
 
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
-TOKEN + Config.TOKEN
+TOKEN = Config.TOKEN
+BOT_ID = Config.BOT_ID
 
 cb = Client("Chatbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
